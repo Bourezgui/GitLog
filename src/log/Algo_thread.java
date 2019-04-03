cf16b6133f7bbe4b116d9cf373c10198d7cb9f08
khalilBourezgui <bourezgui_khalil@outlook.com>
2019-04-03 14:49:57 +0200

    Bean_2

--- src/log/Algo_thread.java
+++ src/log/Algo_thread.java
@@ -6,7 +6,7 @@ import java.lang.reflect.Constructor;
 public class Algo_thread {
 	public static void main(String[] args) {
 		
-		BeanInterface Bean = (BeanInterface) BeanFactory.getBean("log.Bean_2");
+		BeanInterface Bean = (BeanInterface) BeanFactory.getBean("log.Bean_1");
 
 	}
 
