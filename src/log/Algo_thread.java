72e26bbc14eea8a06a76c3402f05d9b048b10551
khalilBourezgui <bourezgui_khalil@outlook.com>
2019-04-03 13:58:46 +0200

    commit

--- src/log/Algo_thread.java
+++ src/log/Algo_thread.java
@@ -17,7 +17,7 @@ public class Algo_thread {
 
 
 class BeanFactory{
-	//Defintion d'un bean statique
+	//Defintion d'un bean statiqueS
 	private static Object Bean ;
 	
 	//cette methode permette d'avoir le bean 
