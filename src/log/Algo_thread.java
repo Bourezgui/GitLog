159f4d5cb2e65b4c6ab0332bf26af893d43f448d
khalilBourezgui <bourezgui_khalil@outlook.com>
2019-04-03 16:25:56 +0200

    master commit

--- src/log/Algo_thread.java
+++ src/log/Algo_thread.java
@@ -6,7 +6,7 @@ import java.lang.reflect.Constructor;
 public class Algo_thread {
 	public static void main(String[] args) {
 		//master modification
-		BeanInterface Bean = (BeanInterface) BeanFactory.getBean("log.Bean_1");
+		BeanInterface Bean = (BeanInterface) BeanFactory.getBean("log.Bean_2");
 		//definition d'une bean
 	}
 
